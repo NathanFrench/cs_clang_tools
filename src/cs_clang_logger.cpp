031505622e141bf5ebe5c05ed0560f65f28b3211
Nathan French <nate@cl0d.com>
2018-04-05 19:56:24 -0400

    derp

--- src/cs_clang_logger.cpp
+++ src/cs_clang_logger.cpp
@@ -1,7 +1,6 @@
 #include <string>
 #include <clang/AST/AST.h>
 #include <clang/AST/ASTConsumer.h>
-#include "clang/Analysis/Analyses/FormatString.h"
 #include <clang/ASTMatchers/ASTMatchFinder.h>
 #include <clang/ASTMatchers/ASTMatchers.h>
 #include <clang/Frontend/CompilerInstance.h>
@@ -18,60 +17,98 @@ using namespace clang::tooling;
 
 static llvm::cl::OptionCategory cs_clang_catagory_("Debugging");
 
-class ReturnStmtHandler: public MatchFinder::MatchCallback {
+class cs_retstmt_handler: public MatchFinder::MatchCallback {
     public:
-        ReturnStmtHandler(clang::Rewriter & Rewrite) : Rewrite(Rewrite) {
+        cs_retstmt_handler(Rewriter & Rewrite) : Rewrite(Rewrite) {
         }
 
-        virtual void run(const MatchFinder::MatchResult &Result) {
-            const clang::ReturnStmt * retStmt = Result.Nodes.getNodeAs < clang::ReturnStmt > ("returnStmt");
+        virtual void run(const MatchFinder::MatchResult& Result) {
+            const ReturnStmt * retStmt =
+                Result.Nodes.getNodeAs < ReturnStmt > ("returnStmt");
 
             if (retStmt) {
-                Rewrite.InsertText(retStmt->getLocStart(), "// log here\n", true, true);
+                Rewrite.InsertText(retStmt->getLocStart(),
+                                   "// log here\n", true, true);
             }
         }
 
     private:
-        clang::Rewriter &Rewrite;
+        Rewriter &Rewrite;
 };
 
-class csASTConsumer: public clang::ASTConsumer {
+class cs_declref_handler: public MatchFinder::MatchCallback {
     public:
-        csASTConsumer(clang::Rewriter & R) : HandleRet(R) {
-            Matcher.addMatcher(returnStmt().bind("returnStmt"), &HandleRet);
+        cs_declref_handler(Rewriter & Rewrite) : Rewrite(Rewrite) {
         }
 
-        void HandleTranslationUnit(clang::ASTContext &Context) override {
+        virtual void run(const MatchFinder::MatchResult& Result) {
+            const FunctionDecl * fDecl =
+                Result.Nodes.getNodeAs < FunctionDecl > ("functionDecl");
+
+            if (fDecl) {
+                Rewrite.InsertText(fDecl->getLocStart(),
+                                   "// log entry here\n", false, false);
+            }
+        }
+
+    private:
+        Rewriter &Rewrite;
+};
+
+class cs_ast_consumer: public ASTConsumer {
+    public:
+        cs_ast_consumer(Rewriter & R) : HandleRet(R), HandleEntry(R) {
+            /* Match any exit points. */
+            Matcher.addMatcher(returnStmt().bind("returnStmt"), &HandleRet);
+            /* Match any executable statements with code. */
+            Matcher.addMatcher(functionDecl(isDefinition()).bind("functionDecl"), &HandleEntry);
+        }
+
+        void HandleTranslationUnit(ASTContext& Context) override {
             Matcher.matchAST(Context);
         }
 
     private:
-        ReturnStmtHandler HandleRet;
-        MatchFinder       Matcher;
+        cs_retstmt_handler HandleRet;
+        cs_declref_handler HandleEntry;
+        MatchFinder        Matcher;
 };
 
-class csFrontendAction: public clang::ASTFrontendAction {
+class cs_frontend_action: public ASTFrontendAction {
     public:
-        csFrontendAction() {
+        cs_frontend_action() {
         }
 
         void EndSourceFileAction() override {
-            TheRewriter.getEditBuffer(TheRewriter.getSourceMgr().getMainFileID()).write(llvm::outs());
+            _rewriter.getEditBuffer(
+                _rewriter.getSourceMgr()
+                .getMainFileID()).write(llvm::outs());
         }
 
-        std::unique_ptr < clang::ASTConsumer > CreateASTConsumer(clang::CompilerInstance &CI, StringRef file) override {
-            TheRewriter.setSourceMgr(CI.getSourceManager(), CI.getLangOpts());
-            return llvm::make_unique < csASTConsumer > (TheRewriter);
+        std::unique_ptr < ASTConsumer >
+        CreateASTConsumer(CompilerInstance & CI, StringRef file) override {
+            _rewriter.setSourceMgr(
+                CI.getSourceManager(),
+                CI.getLangOpts());
+
+            return llvm::make_unique < cs_ast_consumer > (_rewriter);
         }
     private:
-        clang::Rewriter TheRewriter;
+        Rewriter _rewriter;
 };
 
-int main(int argc, const char ** argv) {
+int
+main(int argc, const char ** argv) {
     /* clang::tooling CommonOptionParser */
-    CommonOptionsParser op_parser(argc, argv, cs_clang_catagory_);
-    /* clang::tooling ClangTool, returns ref to loaded compilations db */
-    ClangTool cl_tool(op_parser.getCompilations(), op_parser.getSourcePathList());
+    CommonOptionsParser op_parser(
+        argc,
+        argv,
+        cs_clang_catagory_);
 
-    return cl_tool.run(newFrontendActionFactory < csFrontendAction > ().get());
+    /* clang::tooling ClangTool, returns ref to loaded compilations db */
+    ClangTool cl_tool(
+        op_parser.getCompilations(),
+        op_parser.getSourcePathList());
+
+    return cl_tool.run(newFrontendActionFactory < cs_frontend_action > ().get());
 }
